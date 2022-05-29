0f907077505fb4a51b027a87412cc856cf834c65
zwsimone <zwsimone@gmail.com>
2022-05-29 16:59:42 +0200

    Update firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'wok-stock',
+    appId: '1:651848102037:web:8225fe6fba4c4ff6172b9c',
+    storageBucket: 'wok-stock.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyAtRR08aw0z_F1gPOCS4ua6Um7XamIgp-Q',
+    authDomain: 'wok-stock.firebaseapp.com',
+    messagingSenderId: '651848102037',
+    measurementId: 'G-T0R77G0QHK',
+  },
 	production: true,
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,15 +4,16 @@
 
 export const environment = {
 	production: false,
-	firebase: {
-		apiKey: "AIzaSyAtRR08aw0z_F1gPOCS4ua6Um7XamIgp-Q",
-		authDomain: "wok-stock.firebaseapp.com",
-		projectId: "wok-stock",
-		storageBucket: "wok-stock.appspot.com",
-		messagingSenderId: "651848102037",
-		appId: "1:651848102037:web:8225fe6fba4c4ff6172b9c",
-		measurementId: "G-T0R77G0QHK",
-	},
+  firebase: {
+    projectId: 'wok-stock',
+    appId: '1:651848102037:web:8225fe6fba4c4ff6172b9c',
+    storageBucket: 'wok-stock.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyAtRR08aw0z_F1gPOCS4ua6Um7XamIgp-Q',
+    authDomain: 'wok-stock.firebaseapp.com',
+    messagingSenderId: '651848102037',
+    measurementId: 'G-T0R77G0QHK',
+  },
 };
 
 /*
